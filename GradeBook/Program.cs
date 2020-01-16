7d59329d8e6f09779280efdc93c337559dcc2f63
Caylee Stewart <caylee.stewart@gmail.com>
2020-01-16 18:08:39 -0500

    test

---
+++ GradeBook/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new double[3];
+            numbers[0] = 12.7;
+            numbers[1] = 20.7;
+            numbers[2] = 28.7;
+            var result = numbers[0] + numbers[1] + numbers[2];
+            Console.WriteLine(result);
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
